a06f6f89fb52fc5d11da30b52110da6419f259b5
Oleg Lykasov <oleglykasov@gmail.com>
2017-03-23 01:47:38 +0300

    init commit

---
+++ src/script.js
@@ -0,0 +1,51 @@
+import { filesize } from 'filesize';
+import { throttle } from 'lodash';
+
+const SIZE_ELEM_CLASSNAME = 'snippet__pic-size';
+
+let throttledAddImgInfo = throttle(addImgInfo, 200);
+
+document.addEventListener(
+    'load', event => {
+        if (event.target.tagName == 'IMG')
+            throttledAddImgInfo();
+    }, true);
+
+
+function addImgInfo() {
+    let imgBlocks = document.querySelectorAll('.binary-container .binary');
+    for (let imgBlock of imgBlocks) {
+        let imgElem = imgBlock.querySelector('img');
+
+        if (!imgElem) continue;
+
+        let imgPath = imgElem.src;
+
+        new Promise( (resolve,reject) => {
+            var xhr = new XMLHttpRequest();
+            xhr.open('HEAD', imgPath, true);
+            xhr.onreadystatechange = () => {
+                if ( xhr.readyState == 4 ) {
+                    if ( xhr.status == 200 ) {
+                        resolve(xhr.getResponseHeader('Content-Length'));
+                    } else {
+                        reject('Request error');
+                    }
+                }
+            };
+            xhr.send(null);
+        })
+            .then (imgSize => {
+                let insertionPointElem = imgBlock.querySelector('h5');
+                let sizeElem = insertionPointElem.querySelector(`.${SIZE_ELEM_CLASSNAME}`);
+
+                if(!sizeElem) {
+                    sizeElem = document.createElement('span');
+                    sizeElem.className = SIZE_ELEM_CLASSNAME;
+                    insertionPointElem.appendChild(sizeElem);
+                }
+
+                sizeElem.innerText = ` (${imgElem.naturalWidth}x${imgElem.naturalHeight}, ${filesize(imgSize)})`;
+            });
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const webpack = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const srcDir = `${__dirname}/src`;
+const distDir = `${__dirname}/dist`;
+
+module.exports = {
+    entry: `${srcDir}/script.js`,
+    output: {
+        path: distDir,
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [["es2015", {"modules": false }]]
+
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new CopyWebpackPlugin([{ from: `${srcDir}/manifest.json`, to: distDir }])
+        // new webpack.optimize.UglifyJsPlugin({comments: false})
+    ]
+};
